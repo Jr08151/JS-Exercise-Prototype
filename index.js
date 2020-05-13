9fa10b935f652f2cc3d201d24156f43a8adca291
Jr08151 <Jr08151@users.noreply.github.com>
2020-05-12 20:53:43 -0400

    updated the code from sandbox

--- index.js
+++ index.js
@@ -39,9 +39,24 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = function(someFood) {
+  if (this.stomach.length < 10) this.stomach.push(someFood);
+};
+
+Person.prototype.poop = function() {
+  this.stomach = [];
+};
+
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`;
+};
+
 
 /*
   TASK 2
@@ -57,9 +72,28 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+
+Car.prototype.fill = function(gallons) {
+  this.tank += gallons;
+};
+
+Car.prototype.drive = function(distance) {
+  const range = this.tank * this.milesPerGallon;
+  if (range <= distance) {
+    this.tank = 0;
+    this.odometer += range;
+    return `I ran out of fuel at ${this.odometer} miles!`;
+  }
+  // car did not run out of gas
+  this.odometer += distance;
+  this.tank -= distance * (1 / this.milesPerGallon);
+};
 
 /*
   TASK 3
@@ -68,18 +102,25 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`;
+};
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Global binding: when function is called from the global scope, 'this' refers to the window/console object
+  2. Implicit binding: then a function is called by a preceding dot, 'this' refers to the object left of the dot
+  3. New binding: whenever a constructor function is used, 'this' refers to the specific instance of the object that is being created and returned
+  4. Explicity binding: whenever the call, apply or bind methods are used, 'this' is explicitly defined (it is the first argument passed in);
 */
 
 
